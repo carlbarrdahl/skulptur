3a2a282f2f727c9362454e5d7970ee53e6879d18
Carl Barrdahl <carlbarrdahl@gmail.com>
2021-10-19 16:45:36 +0100

    Show responses in Form and navigate between them

--- pages/forms/[id]/responses.tsx
+++ pages/forms/[id]/responses.tsx
@@ -1,4 +1,5 @@
 import {
+  Divider,
   Table,
   Thead,
   Tr,
@@ -7,11 +8,14 @@ import {
   Td,
   Box,
   Heading,
+  IconButton,
   Flex,
   Textarea,
+  Text,
   Button,
   SkeletonText,
 } from "@chakra-ui/react"
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons"
 
 import { useViewForm, useFormResponses } from "../../../hooks/forms"
 
@@ -19,35 +23,63 @@ import Link from "../../../components/Link"
 import NotAuthorized from "../../../components/NotAuthorized"
 import { useRouter } from "next/router"
 import Container from "../../../components/Container"
+import SchemaForm from "../../../components/SchemaForm"
+import { useState } from "react"
 
-const Header = () => {
-  const router = useRouter()
-  const formId = router.query.id || ""
-  const { isLoading, error, data } = useViewForm(formId)
-
-  return (
-    <Heading size="md" mb={16}>
-      Responses for {data?.title}
-    </Heading>
-  )
-}
 const FormResponsesPage = () => {
   const router = useRouter()
-  const {
-    isLoading,
-    error,
-    data = [],
-    refetch,
-  } = useFormResponses(router.query.id)
+  const res = useFormResponses(router.query.id)
+  const { isLoading, error, data } = useViewForm(router.query.id)
 
-  console.log("errr", error)
+  const [selected, setSelected] = useState(1)
+
+  function handleSelect(value) {
+    setSelected(value)
+  }
+  console.log("responses", res.data)
   return (
     <Container>
-      <Flex justifyContent="flex-end">
-        <Button onClick={() => alert("not implemented")}>Export CSV</Button>
-      </Flex>
-      <Header />
-      <Table>
+      <Flex justifyContent="flex-end"></Flex>
+      <Box mb={8}>
+        <Flex justifyContent="space-between">
+          <Heading size="md" mb={4}>
+            Responses for {data?.title}
+          </Heading>
+          <Flex alignItems="center" mb={4}>
+            <IconButton
+              aria-label="Previous response"
+              icon={<ChevronLeftIcon />}
+              disabled={selected === 1}
+              onClick={() => handleSelect(selected - 1)}
+            />
+
+            <Text px={8}>
+              Response {selected} of {res?.data?.length}
+            </Text>
+            <IconButton
+              aria-label="Next response"
+              icon={<ChevronRightIcon />}
+              disabled={selected === res?.data?.length}
+              onClick={() => handleSelect(selected + 1)}
+            />
+          </Flex>
+          <Button onClick={() => alert("not implemented")}>Export CSV</Button>
+        </Flex>
+        {!isLoading && res.data ? (
+          <SchemaForm
+            schema={data?.schema}
+            formData={JSON.parse(res.data[selected - 1].content.data)}
+            onSubmit={console.log}
+          >
+            {" "}
+          </SchemaForm>
+        ) : (
+          "..."
+        )}
+      </Box>
+      <Divider mb={8} />
+      <Heading size={"md"}>All responses</Heading>
+      <Table mb={16}>
         <Thead>
           <Tr>
             <Th>DID</Th>
@@ -56,7 +88,7 @@ const FormResponsesPage = () => {
           </Tr>
         </Thead>
         <Tbody>
-          {isLoading ? (
+          {res.isLoading ? (
             <Tr>
               <Td>
                 <SkeletonText noOfLines={1} />
@@ -68,14 +100,14 @@ const FormResponsesPage = () => {
                 <SkeletonText noOfLines={1} />
               </Td>
             </Tr>
-          ) : error?.message.includes("not authenticated") ? (
+          ) : res.error?.message.includes("not authenticated") ? (
             <Tr>
               <Td colSpan={3}>
-                <NotAuthorized retry={refetch} />
+                <NotAuthorized retry={res.refetch} />
               </Td>
             </Tr>
           ) : (
-            data.map((response, id) => {
+            (res.data || []).map((response, id) => {
               return (
                 <Tr key={id}>
                   <Td>{response.metadata.controllers[0]}</Td>
