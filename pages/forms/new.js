b04638d8e8311db6f489140ca2d14c530b38ef5e
Carl Barrdahl <carlbarrdahl@gmail.com>
2021-10-20 12:21:15 +0100

    Create form spinner

--- pages/forms/new.js
+++ pages/forms/new.js
@@ -6,6 +6,7 @@ import {
   Flex,
   Textarea,
   Select,
+  Spinner,
 } from "@chakra-ui/react"
 
 import { useEffect, useState } from "react"
@@ -15,24 +16,6 @@ import Container from "../../components/Container"
 
 import SchemaForm from "../../components/SchemaForm"
 import { useCreateForm } from "../../hooks/forms"
-
-const initialSchema = {
-  title: "Skulptur Form Example",
-  description: `This is an example of how a form could look like. You can edit the form to the left and immediately see the changes reflected here.
-
-When you click Create Form a few things will happen:
-- A Ceramic Schema based on the json-schema you provided will be created
-- A Ceramic Tile is created with the title, description and the schema
-- The created Form id is added to your definitions index
-
-You can now copy the share link and send to your friend.`,
-  type: "object",
-  required: ["title"],
-  properties: {
-    title: { type: "string", title: "Title", default: "A new task" },
-    done: { type: "boolean", title: "Done?", default: false },
-  },
-}
 
 const examples = [
   {
@@ -119,77 +102,83 @@ export default function NewFormPage() {
     })
   }
 
-  if (isLoading) {
-    return <pre>Creating form...</pre>
-  }
-
   return (
     <Container>
-      <Flex justifyContent="space-between" mb={4}>
-        <Box>
-          <Select
-            onChange={(e) => {
-              setSchema(
-                JSON.stringify(
-                  examples.find((ex) => ex.title === e.target.value),
-                  null,
-                  2
-                )
-              )
-            }}
-          >
-            {examples.map((ex, i) => (
-              <option key={i} name={i} value={ex.title}>
-                {ex.title}
-              </option>
-            ))}
-          </Select>
-        </Box>
-        <Box>
-          <Button onClick={() => handleCreateForm(JSON.parse(schema))}>
-            Create Form
-          </Button>
-        </Box>
-      </Flex>
+      {" "}
+      {isLoading ? (
+        <Flex justifyContent="center">
+          <Spinner mr={4} />
+          <Text>Creating form...</Text>
+        </Flex>
+      ) : (
+        <div>
+          <Flex justifyContent="space-between" mb={4}>
+            <Box>
+              <Select
+                onChange={(e) => {
+                  setSchema(
+                    JSON.stringify(
+                      examples.find((ex) => ex.title === e.target.value),
+                      null,
+                      2
+                    )
+                  )
+                }}
+              >
+                {examples.map((ex, i) => (
+                  <option key={i} name={i} value={ex.title}>
+                    {ex.title}
+                  </option>
+                ))}
+              </Select>
+            </Box>
+            <Box>
+              <Button onClick={() => handleCreateForm(JSON.parse(schema))}>
+                Create Form
+              </Button>
+            </Box>
+          </Flex>
 
-      <Flex flex={1} minW="100%" height={"70vh"}>
-        <Box width={["100%"]}>
-          <Heading
-            size="xs"
-            textTransform="uppercase"
-            letterSpacing="wider"
-            color="gray.500"
-          >
-            Editor
-          </Heading>
+          <Flex flex={1} minW="100%" height={"70vh"}>
+            <Box width={["100%"]}>
+              <Heading
+                size="xs"
+                textTransform="uppercase"
+                letterSpacing="wider"
+                color="gray.500"
+              >
+                Editor
+              </Heading>
 
-          <Textarea
-            style={{ fontFamily: "monospace" }}
-            width="100%"
-            height="100%"
-            onChange={handleChange}
-            value={schema}
-          ></Textarea>
-        </Box>
-        <Box width={["100%"]} pl={4}>
-          <Heading
-            size="xs"
-            textTransform="uppercase"
-            letterSpacing="wider"
-            color="gray.500"
-          >
-            Preview
-          </Heading>
-          <FormWithError
-            schema={schema}
-            onChange={() => console.log("changed")}
-            onSubmit={() => console.log("submitted")}
-            onError={() => console.log("errors")}
-          >
-            {" "}
-          </FormWithError>
-        </Box>
-      </Flex>
+              <Textarea
+                style={{ fontFamily: "monospace" }}
+                width="100%"
+                height="100%"
+                onChange={handleChange}
+                value={schema}
+              ></Textarea>
+            </Box>
+            <Box width={["100%"]} pl={4}>
+              <Heading
+                size="xs"
+                textTransform="uppercase"
+                letterSpacing="wider"
+                color="gray.500"
+              >
+                Preview
+              </Heading>
+              <FormWithError
+                schema={schema}
+                onChange={() => console.log("changed")}
+                onSubmit={() => console.log("submitted")}
+                onError={() => console.log("errors")}
+              >
+                {" "}
+              </FormWithError>
+            </Box>
+          </Flex>
+        </div>
+      )}
     </Container>
   )
 }
